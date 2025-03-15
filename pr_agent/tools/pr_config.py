507cd6e675be6cdd9ce7a8853e59041b238ff565
mrT23 <tal.r@codium.ai>
2025-03-15 09:56:57 +0200

    skip in PR config

--- pr_agent/tools/pr_config.py
+++ pr_agent/tools/pr_config.py
@@ -41,7 +41,8 @@ class PRConfig:
         skip_keys = ['ai_disclaimer', 'ai_disclaimer_title', 'ANALYTICS_FOLDER', 'secret_provider', "skip_keys", "app_id", "redirect",
                      'trial_prefix_message', 'no_eligible_message', 'identity_provider', 'ALLOWED_REPOS',
                      'APP_NAME', 'PERSONAL_ACCESS_TOKEN', 'shared_secret', 'key', 'AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'user_token',
-                     'private_key', 'private_key_id', 'client_id', 'client_secret', 'token', 'bearer_token']
+                     'private_key', 'private_key_id', 'client_id', 'client_secret', 'token', 'bearer_token', 'jira_api_token','webhook_secret']
+        partial_skip_keys = ['key', 'secret', 'token', 'private']
         extra_skip_keys = get_settings().config.get('config.skip_keys', [])
         if extra_skip_keys:
             skip_keys.extend(extra_skip_keys)
@@ -56,6 +57,8 @@ class PRConfig:
                 markdown_text += f"==================== {header} ===================="
             for key, value in configs.items():
                 if key.lower() in skip_keys_lower:
+                    continue
+                if any(skip_key in key.lower() for skip_key in partial_skip_keys):
                     continue
                 markdown_text += f"\n{header.lower()}.{key.lower()} = {repr(value) if isinstance(value, str) else value}"
                 markdown_text += "  "
